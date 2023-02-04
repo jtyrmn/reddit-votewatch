af9d918361a4fccd68ed1a2335f2ada754b73ddb
jtyrmn <jaketyermanbusiness@gmail.com>
2023-02-03 17:21:13 -0800

    allowed env file to be accessed from ENV_PATH environment variable

--- main.go
+++ main.go
@@ -1,28 +1,32 @@
 package main
 
 import (
-	"fmt"
 	"log"
+	"os"
 
 	"github.com/joho/godotenv"
 	"github.com/jtyrmn/reddit-votewatch/database"
 	"github.com/jtyrmn/reddit-votewatch/reddit"
 	"github.com/jtyrmn/reddit-votewatch/scheduler"
-	"github.com/jtyrmn/reddit-votewatch/util"
 )
 
 func main() {
 	//load env variables
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("error loading .env file")
+	envPath := ".env"
+	if e, exists := os.LookupEnv("ENV_PATH"); exists {
+		envPath = e
 	}
 
+	err := godotenv.Load(envPath)
+	if err != nil {
+		log.Fatal("error loading .env file: " + err.Error())
+	}
+
+	// init APIs to reddit and database
 	r, err := reddit.Connect()
 	if err != nil {
 		log.Fatal("error connecting to reddit:\n" + err.Error())
 	}
-	fmt.Println(r)
 
 	database, err := database.Connect()
 	if err != nil {
@@ -30,13 +34,4 @@ func main() {
 	}
 
 	scheduler.Start(r, database)
-
-	fmt.Println(util.GetEnvInt("YEHEBOI"))
-
-	// f := reddit.Fullname("t3_v7zzrm")
-	// d, _ := r.GetNewestPosts("wallstreetbets", 20, &f)
-	// for _, p := range d {
-	// 	fmt.Printf("%s: %s\n", p.FullId(), p.Title)
-	// }
-	// fmt.Println(len(d))
 }
